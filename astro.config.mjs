af1f6181da09677ca036a848b93687d4a149e6f8
niksram <nikhilsram.off@gmail.com>
2024-05-10 09:31:14 +0000

    deploying:github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://niksram.github.io',
-  base: 'niksram.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
